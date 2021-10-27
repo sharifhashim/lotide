64a196097a3cb873eb509d7ca2f0545721ac2dcf
Hashim Sharif <sharif.hashim@gmail.com>
2021-10-27 22:03:29 +0000

    function return all indices in string where each character is found

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const eqArrays = function(arr1, arr2) {
+  let x = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    let value1 = arr1[i];
+    do {
+      let value2 = arr2[x];
+      if (value1 !== value2) {
+        return false;
+      } else if (value1 === value2) {
+        x = x + 1;
+        break;
+      }
+    }
+    while (x < arr2.length);
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (!eqArrays(arr1, arr2)) {
+    console.log(`🛑🛑🛑 Assertion Failed`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed`);
+  }
+};
+
+
+const letterPositions = function(sentences) {
+  const results = {};
+  for (let i = 0; i < sentences.length; i++) {
+    if (sentences[i] === " ") {
+      continue;
+    }
+    if (results[sentences[i]]) {
+      results[sentences[i]].push(i);
+    } else {
+      results[sentences[i]] = [i];
+    }
+    
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").e, [1])
