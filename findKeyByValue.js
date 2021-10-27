de00b8ea0d973f92b332163252964290f0d196b5
Hashim Sharif <sharif.hashim@gmail.com>
2021-10-27 22:09:34 +0000

    function console.logs object keys

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(Obj, Value) {
+  for (let item in Obj) {
+    console.log(item)
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+findKeyByValue(bestTVShowsByGenre)
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
