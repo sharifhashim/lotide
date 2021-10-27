51b53e18ed56473e9d2ec85c1417e3aa71006996
Hashim Sharif <sharif.hashim@gmail.com>
2021-10-27 17:21:42 +0000

    middle function returns middle two numbers when array.length is even number

--- middle.js
+++ middle.js
@@ -32,6 +32,11 @@ const middle = function(arr) {
     let mid = Math.floor(arr.length / 2)
     newArr.push(arr[mid])
     return newArr
+  } else if (arr.length % 2 === 0) {
+    let middleTwo = arr.length / 2
+    newArr.push(arr[middleTwo - 1]) 
+    newArr.push(arr[middleTwo])
+    return newArr
   }
 }
 
@@ -39,3 +44,7 @@ console.log(middle([]))
 console.log(middle([1]))
 console.log(middle([1,2]))
 console.log(middle([1,2,3,4,5]))
+console.log(middle([1,2,3,4,5,6]))
+console.log(middle([1,2,3,4,5,6,7,8]))
+
+assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [1,2,3,4,5,6,7,8])
