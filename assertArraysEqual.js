2bb47d5b4ec0917b4b9e85a6fa1369c55ea9d334
Hashim Sharif <sharif.hashim@gmail.com>
2021-10-27 13:54:33 +0000

    change console.log in assertArrayEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -18,9 +18,9 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (!eqArrays(arr1, arr2)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`)
+    console.log(`🛑🛑🛑 Assertion Failed`)
   } else {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+    console.log(`✅✅✅ Assertion Passed`)
   }
 }
 
