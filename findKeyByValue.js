6b271710cd2d472be5a0075a4ecba93ff4744f05
Hashim Sharif <sharif.hashim@gmail.com>
2021-10-28 02:25:52 +0000

    add test cases bottom of code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -20,8 +20,10 @@ const bestTVShowsByGenre = {
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
+
 console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"))
 console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"))
 console.log(findKeyByValue(bestTVShowsByGenre, "Cops"))
+
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
