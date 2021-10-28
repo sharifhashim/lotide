e75951ee320a63270982fa878e2c58bc6b14ae16
Hashim Sharif <sharif.hashim@gmail.com>
2021-10-28 13:37:35 +0000

    change console.log statement in assertArrayEq function to display array in console

--- middle.js
+++ middle.js
@@ -21,9 +21,9 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (!eqArrays(arr1, arr2)) {
-    console.log(`🛑🛑🛑 Assertion Failed`)
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`)
   } else {
-    console.log(`✅✅✅ Assertion Passed`)
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
   }
 }
 
